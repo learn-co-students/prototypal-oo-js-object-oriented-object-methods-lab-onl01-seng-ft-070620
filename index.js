fef636f37965be83f7d59a0e645c14c174c35c5a
Amanda-Katherine <walker.akw@gmail.com>
2020-10-23 19:58:14 -0400

    Done.

--- index.js
+++ index.js
@@ -5,7 +5,21 @@
  }
 
  BoardMember.prototype.veto = function() {
-     return "No I must disagree"
+     return "No, I must disagree"
  }
 
- veto, approve, doCharity, releasePressStatement, sayHi
+ BoardMember.prototype.approve = function() {
+    return "You can do that!"
+ }
+
+ BoardMember.prototype.doCharity = function() {
+    return "I like to help people." 
+}
+
+BoardMember.prototype.releasePressStatement = function() {
+     return "You will see great things from Scuber."
+}
+
+BoardMember.prototype.sayHi = function() {
+     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+}
